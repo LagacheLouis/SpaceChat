9cafbc9f40a45cfe62cbea128ec50f2cdfcdf0ed
LACOTTE Robinson <rlacotte@g-a102b-08.local>
2018-09-13 10:23:20 +0200

    form compo

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <form action="" @submit.prevent="onFormSubmit">
+            <input type="text" placeholder="Pseudooooo" v-model="username">
+            <button>Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: ''
+        }
+    },
+    methods: {
+        onFormSubmit() {
+            if (this.username.length > 0) {
+                this.$emit('submit', this.username)
+            } else {
+
+            }
+        }
+    }
+}
+</script>
+
