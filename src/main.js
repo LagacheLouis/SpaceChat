17d0f0bafd4df7af9a6b44aeef62e50bb393412c
LAGACHE Louis <llagache@g-a102b-09.local>
2018-09-14 11:00:23 +0200

    debug de tes mort (css)

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import api from './plugins/api'
+
+Vue.config.productionTip = false;
+
+Vue.use(api);
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
